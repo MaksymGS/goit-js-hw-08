732f221051ca053ab6086684b06f55ba6c1dc83d
MaksymGS <130307586+MaksymGS@users.noreply.github.com>
2023-08-22 21:17:11 +0300

    all done fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,25 +1,27 @@
+
 import throttle from 'lodash.throttle';
 const fbForm = document.querySelector('.feedback-form');
 fbForm.addEventListener('input', throttle(handlerForm, 500));
 fbForm.addEventListener('submit', handlerSubmit);
 
-let formData;
-try {
-  formData = JSON.parse(localStorage.getItem('feedback-form-state')) ?? {
-    email: '',
-    message: '',
-  };
-} catch (error) {
-  console.log(error.message);
+let formData
+try {formData = JSON.parse(localStorage.getItem('feedback-form-state')) ?? {
+  email: "",
+  message: ""
+};
+  } catch (error) {
+   console.log(error.message);
 }
 const { email, message } = fbForm.elements;
-email.value = formData.email;
-message.value = formData.message;
+  email.value = formData.email ?? "";
+  message.value = formData.message ?? "";
+
 
 function handlerForm(evt) {
   evt.preventDefault();
   formData[evt.target.name] = evt.target.value;
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+
 }
 function handlerSubmit(evt) {
   evt.preventDefault();
